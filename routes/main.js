240b23cab39d2d124efefa57cbb6b116713dc074
Paweł Chochura <pawel.chochura@g2a.local>
2020-05-18 16:58:30 +0200

    Added popup

--- routes/main.js
+++ routes/main.js
@@ -1,3 +1,65 @@
 module.exports = (_, res) => {
-	res.render('main.ejs');
+	res.render('main.ejs', {
+		rooms: [
+			{
+				name: 'Paweł',
+				lists: [
+					{
+						name: 'Asia',
+						notes: [
+							'Notka 3',
+						],
+					},
+					{
+						name: 'Konrad',
+						notes: [
+							'Notka 1',
+							'Notka 2'
+						]
+					},
+				],
+				ready: true
+			},
+			{
+				name: 'Asia',
+				lists: [
+					{
+						name: 'Paweł',
+						notes: [
+							'Notka 1',
+							'Notka 2'
+						]
+					},
+					{
+						name: 'Konrad',
+						notes: [
+							'Notka 1',
+							'Notka 2'
+						]
+					},
+				],
+				ready: false
+			},
+			{
+				name: 'Konrad',
+				lists: [
+					{
+						name: 'Paweł',
+						notes: [
+							'Notka 1',
+							'Notka 2'
+						]
+					},
+					{
+						name: 'Asia',
+						notes: [
+							'Notka 1',
+							'Notka 2'
+						]
+					},
+				],
+				ready: false
+			}
+		],
+	});
 };
